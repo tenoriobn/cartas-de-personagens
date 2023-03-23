2312b60b5ffb9b13268765c1c9bd8e96ed6b7e85
Bruno Tenorio <tenoriobn@gmail.com>
2023-03-23 15:38:37 -0300

    Divindo as responsabilidades das classes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
 import { Personagem } from "./modules/personagem.js";
 
-/*const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}*/
-
-/*const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}*/
-
-const personagemPedrinho = new Personagem();
-personagemPedrinho.nome = 'Pedrinho';
-personagemPedrinho.mana = 12;
-personagemPedrinho.vida = 7;
-personagemPedrinho.tipo = 'Mago';
-personagemPedrinho.level = 5;
-
-const personagemJose = new Personagem();
-personagemJose.nome = 'Jose';
-personagemJose.tipo = 'Arqueiro';
-personagemJose.level = 3;
-
-console.log('Insignia de ' + personagemPedrinho.nome + ': ' + personagemPedrinho.obterInsignia());
-console.log('Insignia de ' + personagemJose.nome + ': ' + personagemJose.obterInsignia());
+const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
