44f35ed430f1e2b6f62c5785302ca16036efe6ac
Bruno Tenorio <tenoriobn@gmail.com>
2023-03-31 16:23:06 -0300

    Criando personangem Arqueiro-Mago utilizando composição

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3);
 const magoJulia = new Mago('Julia', 8, 'Gelo', 7, 10);
 const arqueiroBruno = new Arqueiro('Bruno', 7, 15);
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magoJulia, arqueiroBruno];
+const personagens = [magoAntonio, magoJulia, arqueiroBruno, arqueiroMagoChico];
 
 new PersonagemView(personagens).render()
 
