d64cfe530a4d1a503cfa96b10420aa76450ec452
Bruno Tenorio <tenoriobn@gmail.com>
2023-04-04 11:25:13 -0300

    Criando a 'static descricao'

--- src/modules/arqueiro.js
+++ src/modules/arqueiro.js
@@ -3,6 +3,7 @@ import { Personagem } from "./personagem.js";
 export class Arqueiro extends Personagem {
     static tipo = 'Arqueiro'
     destreza
+    static descricao = 'Você tem o meu arco!'
 
     constructor(nome, level, destreza) {
         super(nome, level);
